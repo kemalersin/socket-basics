c0d314c3a188c9dd249059f0fcbd16815b334cf7
Kemal Ersin Yılmaz <mail@kemalersin.com>
2016-02-22 16:46:52 +0200

    Add @currentUsers command

--- server.js
+++ server.js
@@ -9,6 +9,29 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+function sendCurrentUsers(socket) {
+    var info = clientInfo[socket.id];
+    var users = [];
+
+    if (typeof info === 'undefined') {
+        return;
+    }
+
+    Object.keys(clientInfo).forEach(function (socketId) {
+        var userInfo = clientInfo[socketId];
+
+        if (info.room = userInfo.room) {
+            users.push(userInfo.name);
+        }
+    });
+
+    socket.emit('message', {
+        name: 'System',
+        text: 'Current users: ' + users.join(', '),
+        timestamp: moment().valueOf()
+    });
+}
+
 io.on('connection', function (socket) {
     console.log('User connected via socket.io!');
 
@@ -17,6 +40,7 @@ io.on('connection', function (socket) {
 
         if (typeof userData != 'undefined') {
             socket.leave(userData.room);
+
             io.to(userData.room).emit('message', {
                 name: 'System',
                 text: userData.name + ' has left!',
@@ -30,6 +54,7 @@ io.on('connection', function (socket) {
     socket.on('joinRoom', function (req) {
         clientInfo[socket.id] = req;
         socket.join(req.room);
+
         socket.broadcast.to(req.room).emit('message', {
             name: 'System',
             text: req.name + ' has joined!',
@@ -40,8 +65,12 @@ io.on('connection', function (socket) {
     socket.on('message', function (message) {
         console.log('Message received: ' + message.text);
 
-        message.timestamp = moment().valueOf();
-        io.to(clientInfo[socket.id].room).emit('message', message);
+        if (message.text === '@currentUsers') {
+            sendCurrentUsers(socket);
+        } else {
+            message.timestamp = moment().valueOf();
+            io.to(clientInfo[socket.id].room).emit('message', message);
+        }
     });
 
     socket.emit('message', {
